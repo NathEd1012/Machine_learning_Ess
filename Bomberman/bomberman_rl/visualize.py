1eebb593ce119d9c01e3001301622669cfc24afb
juanc.prolam <juanc.prolam@gmail.com>
2024-09-05 17:18:34 +0200

    Changes to tests, visualize.py

--- Bomberman/bomberman_rl/visualize.py
+++ Bomberman/bomberman_rl/visualize.py
@@ -46,15 +46,56 @@ def plot_event_statistics(data, stats_to_plot):
 
     plt.show()
 
+def plot_action_histogram(data, actions_to_plot, row = -1):
+    """
+    Plot a histogram of the action distribution of selected row
+    """
+    if row >= len(data) or row < -len(data):
+        print(f"Invalid row index: {row}. Data only has {len(data)} rows")
+        sys.exit(1)
+
+    row_data = data.iloc[row]
+
+    action_counts = []
+    for action in actions_to_plot:
+        if action in data.columns:
+            action_counts.append(row_data[action])
+        else:
+            print(f"Action {action} not found in columns.")
+
+    plt.figure(figsize = (10, 6))
+    plt.bar(actions_to_plot, action_counts, color = 'skyblue')
+    plt.title(f"Action distribution for Row {row if row >= 0 else 'Last' }")
+    plt.xlabel("Action")
+    plt.ylabel("Count")
+    plt.tight_layout()
+
+    plt.show()
+
 def main():
     parser = argparse.ArgumentParser(description = "Visualize training statistics.")
     parser.add_argument('--file-path', type = str, required = True, help = "Path to the training_stats.csv file.")
-    parser.add_argument('--stats', nargs = '+', required = True, help = "Select statistics to visualize")
+
+    # Time series
+    parser.add_argument('--stats', nargs = '+', help = "Select statistics to visualize over training iterations")
+
+    # Histogram
+    parser.add_argument('--hist', nargs = '*', help = "Select action counts to visualize in histogram")
+    parser.add_argument('--row', type = int, default = -1, help = "Row index for histogram. Default: Last row")
 
     args = parser.parse_args()
 
     data = load_data(args.file_path)
-    plot_event_statistics(data, args.stats)
+
+    # Stats plot?
+    if args.stats:
+        plot_event_statistics(data, args.stats)
+
+    # Hist?
+    default_actions = ["MOVED_UP", "MOVED_DOWN", "MOVED_RIGHT", "MOVED_LEFT", "BOMB_DROPPED", "WAITED", "INVALID_ACTION"]
+    if args.hist is not None:
+        actions_to_plot = args.hist if args.hist else default_actions
+        plot_action_histogram(data, actions_to_plot, row = args.row)
 
 if __name__ == "__main__":
     main()
